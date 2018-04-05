54ecef795d1ea92319b53db52c00968768857cfa
Jordan Van Ness <jordanwvn@gmail.com>
2018-04-05 15:40:09 -0700

    adding judge route circle.

---
+++ judge-route-circle.js
@@ -0,0 +1,41 @@
+/* JUDGE ROUTE CIRCLE **********************************************************
+Initially, there is a Robot at position (0, 0). Given a sequence of its moves,
+judge if this robot makes a circle, which means it moves back to the original
+place.
+
+The move sequence is represented by a string. And each move is represent by a
+character. The valid robot moves are R (Right), L (Left), U (Up) and D (down).
+The output should be true or false representing whether the robot makes a
+circle.
+
+Example 1:
+- Input: "UD"
+- Output: true
+
+Example 2:
+- Input: "LL"
+- Output: false
+*******************************************************************************/
+
+var judgeCircle = function(moves) {
+    let x = 0;
+    let y = 0;
+
+    for (let direction of moves) {
+        switch (direction) {
+        case 'U':
+            y++;
+            break;
+        case 'D':
+            y--;
+            break;
+        case 'R':
+            x++;
+            break;
+        case 'L':
+            x--;
+            break;
+        }
+    }
+    return (!x && !y) ? true : false;
+};
