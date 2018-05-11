4bc5579fd7bcb5d540d0446a6777e817f8e9a96a
Jordan Van Ness <jordanwvn@gmail.com>
2018-05-11 11:22:58 -0700

    adding largest number at least twice of others.

---
+++ largest-number-at-least-twice-of-others.js
@@ -0,0 +1,37 @@
+/* LARGEST NUMBER AT LEAST TWICE OF OTHERS *************************************
+In a given integer array nums, there is always exactly one largest element.
+
+Find whether the largest element in the array is at least twice as much as
+every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+
+Example 1:
+
+- Input: nums = [3, 6, 1, 0]
+- Output: 1
+Explanation: 6 is the largest integer, and for every other number in the array
+x,
+6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+
+
+Example 2:
+
+- Input: nums = [1, 2, 3, 4]
+- Output: -1
+Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+*******************************************************************************/
+
+var dominantIndex = function(nums) {
+    let largest = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[largest]) largest = i;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i === largest) i++;
+        if (!((nums[i] << 1) <= nums[largest])) return -1;
+    }
+    return largest;
+};
