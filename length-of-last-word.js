9e14c607da2df05ddf3461760d9586428617352f
Jordan Van Ness <jordanwvn@gmail.com>
2018-06-06 13:23:30 -0700

    adding length of last words.

---
+++ length-of-last-word.js
@@ -0,0 +1,18 @@
+/* LENGTH OF LAST WORD *********************************************************
+Given a string s consists of upper/lower-case alphabets and empty space
+characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space
+characters only.
+
+Example:
+- Input: "Hello World"
+- Output: 5
+*******************************************************************************/
+
+var lengthOfLastWord = function(s) {
+    let sArr = s.trim().split(' ');
+    return sArr[sArr.length - 1].length;
+};
