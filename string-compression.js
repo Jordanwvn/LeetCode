1b5ff12e769937caafc4d8fff50a88fb7880f726
Jordan Van Ness <jordanwvn@gmail.com>
2018-06-06 13:03:10 -0700

    adding string compression.

---
+++ string-compression.js
@@ -0,0 +1,67 @@
+/* STRING COMPRESSION **********************************************************
+Given an array of characters, compress it in-place.
+
+The length after compression must always be smaller than or equal to the
+original array.
+
+Every element of the array should be a character (not int) of length 1.
+
+After you are done modifying the input array in-place, return the new length of
+the array.
+
+Follow up:
+Could you solve it using only O(1) extra space?
+
+Example 1:
+- Input:
+["a","a","b","b","c","c","c"]
+
+- Output:
+Return 6, and the first 6 characters of the input array should be:
+["a","2","b","2","c","3"]
+
+Explanation:
+"aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+
+Example 2:
+- Input:
+["a"]
+
+- Output:
+Return 1, and the first 1 characters of the input array should be: ["a"]
+
+Explanation:
+Nothing is replaced.
+
+Example 3:
+- Input:
+["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+
+- Output:
+Return 4, and the first 4 characters of the input array should be:
+["a","b","1","2"].
+
+Explanation:
+Since the character "a" does not repeat, it is not compressed. "bbbbbbbbbbbb"
+is replaced by "b12".
+
+Notice each digit has it's own entry in the array.
+*******************************************************************************/
+
+var compress = function(chars) {
+    let count = 1;
+
+    for (let i = chars.length - 1; i >= 0; i--) {
+        if (chars[i] === chars[i - 1]) {
+            count++;
+            chars.splice(i, 1);
+        } else if (count !== 1) {
+            let countArr = [...`${count}`];
+            countArr.reverse().forEach(num => {
+                chars.splice(i + 1, 0, num);
+            })
+            count = 1;
+        }
+    }
+    return chars.length;
+};
