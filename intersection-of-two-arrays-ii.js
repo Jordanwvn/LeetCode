5560b6fcb9683a6e25b390837c6f5ab49691df14
Jordan Van Ness <jordanwvn@gmail.com>
2018-06-05 18:03:48 -0700

    adding intersection of two arrays II.

--- intersection-of-two-arrays-ii.js
+++ intersection-of-two-arrays-ii.js
@@ -5,17 +5,19 @@ Example:
 Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
 *******************************************************************************/
 
-let nums = {};
+var intersect = function(nums1, nums2) {
+    let nums = {};
 
-for (let num of nums1) nums[num] = nums[num] + 1 || 1;
+    for (let num of nums1) nums[num] = nums[num] + 1 || 1;
 
-let result = [];
+    let result = [];
 
-for (let num of nums2) {
-    if (nums[num]) {
-        result.push(num);
-        nums[num]--;
+    for (let num of nums2) {
+        if (nums[num]) {
+            result.push(num);
+            nums[num]--;
+        }
     }
-}
 
-return result;
+    return result;
+};
