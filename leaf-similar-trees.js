80d06262f88c4529624b252282b4cf06c9503190
Jordan Van Ness <jordanwvn@gmail.com>
2018-07-30 18:46:02 -0700

    adding leaf-similar trees in JS.

---
+++ leaf-similar-trees.js
@@ -0,0 +1,35 @@
+/* LEAF-SIMILAR TREES **********************************************************
+
+Consider all the leaves of a binary tree.  From left to right order, the values
+of those leaves form a leaf value sequence.
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9,
+8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is
+the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2
+are leaf-similar.
+*******************************************************************************/
+
+var leafSimilar = function(root1, root2) {
+    let leaves = [];
+    let result = true;
+
+    let traverse = function (node, cb) {
+        if (node.left) traverse(node.left, cb);
+        if (node.right) traverse(node.right, cb);
+        if (!node.left && !node.right) cb(node);
+    }
+
+    traverse(root1, node => {leaves.push(node.val)});
+
+    let count = 0;
+    traverse(root2, node => {
+        if (leaves[count] !== node.val) result = false;
+        count++;
+    });
+
+    return result;
+}
